b5160cf7d24bc562e250a4d7b2b00600e9c01f09
Francosor <153915595+Francosor@users.noreply.github.com>
2025-05-13 17:03:33 -1000

    Update index.js

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.post("/", function(req, res) {
     
     //build up the URL for the JSON query, API Key is // secret and needs to be obtained by signup 
         const units = "imperial";
-        const apiKey = "67f6b382921c1e89b39b20d4f9556f22";
+        const apiKey = "f8196efaf8ed3e3c13c78471fa20869d";
         const url = "https://api.openweathermap.org/data/2.5/weather?zip=" + zip +  "&units=" + units + "&APPID=" + apiKey;
     
     // this gets the data from Open WeatherPI
